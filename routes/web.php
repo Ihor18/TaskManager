92321f23b00d5356c91c2bacf9cb7f6b3e63445d
Ihor Khalupniak <58263624+Ihor18@users.noreply.github.com>
2021-06-28 16:53:40 +0300

    Delete unnecessary route

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('page');
 })->name('Tasks');
-Route::get('/todos/{id}', function () {
-    return view('page');
-})->name('Todos');
 
 
 
